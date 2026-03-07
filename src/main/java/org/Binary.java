360fce794c5d575e393487368ecda96d16cb9019
Abhinav Sarkar <abhi.sarkar.anu@gmail.com>
2026-03-07 15:30:04 +0000

    Refactor binary conversion logic
    Replaces the separate `convert` and `rev` methods with a single, more
    efficient `toBinary` method.
    Also, removes the unused `TicTacToe` class.

--- src/main/java/org/Binary.java
+++ src/main/java/org/Binary.java
@@ -4,37 +4,25 @@ import java.util.Scanner;
 
 public class Binary {
 
-    public static void main(final String[] args) {
+    public static int toBinary(int n) {
+        String binary = "";
+        int temp;
+        while (n != 0) {
+            temp = n % 2;
+            binary = temp + binary;
+            n /= 2;
+        }
+        return Integer.parseInt(binary);
+    }
 
-        final Scanner in = new Scanner(System.in);
-        System.out.print("Enter a number in decimal: ");
-        final long n = in.nextLong();
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number in decimal");
+        int n = in.nextInt();
 
-        final String m = convert(n);
-        final String bi = rev(m);
-        final long binary = Integer.parseInt(bi);
+        int binary = toBinary(n);
         System.out.println(binary);
 
         in.close();
-
-    }
-
-    static String rev(final String str) {
-
-        if (str == null || str.isEmpty() || str.length() == 1) {
-            return str;
-        }
-        return rev(str.substring(1)) + str.charAt(0);
-    }
-
-    static String convert(long n) {
-        String bi = "";
-
-        while (n > 0) {
-            bi += n % 2;
-            n /= 2;
-        }
-
-        return bi;
     }
 }
